64cac87c34770024b4474c25a6820aad0de106b3
David Álvarez <dabidop@gmail.com>
2025-03-04 22:07:15 -0500

    ok

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [vue()], // Asegura que Vite maneje archivos .vue
+  plugins: [vue()],
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173
+  },
+  preview: {
+    allowedHosts: ['star-wars-7hvc.onrender.com']
   }
 });
